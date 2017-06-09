e292d2c710a1185fad1900ca9a77e8843aa83abd
Matthias Bargmann <m@bargmann.com.de>
2017-06-09 14:19:00 +0200

    implement version commandline scanner

--- src/test/java/com/github/mazzeb/gradle/autoversion/InteractiveVersionReaderTest.java
+++ src/test/java/com/github/mazzeb/gradle/autoversion/InteractiveVersionReaderTest.java
@@ -2,31 +2,32 @@ package com.github.mazzeb.gradle.autoversion;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
 
+import static com.github.mazzeb.gradle.autoversion.InteractiveVersionReader.interactiveVersionReader;
 import static com.github.mazzeb.gradle.autoversion.Version.versionBuilder;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class InteractiveVersionReaderTest {
 
-    @Mock
     InputStream inputStream;
-
-    @Mock
     PrintStream printStream;
-
-    @InjectMocks
     InteractiveVersionReader interactiveVersionReader;
 
     @Before
     public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
+        inputStream = new ByteArrayInputStream("1.4.2-SNAPSHOT".getBytes());
+        printStream = mock(PrintStream.class);
+        interactiveVersionReader = interactiveVersionReader(inputStream, printStream);
     }
 
     @Test
@@ -60,6 +61,7 @@ public class InteractiveVersionReaderTest {
 
     @Test
     public void shouldParseVersionStringWithLabelAndSomeRandomStuff() throws Exception {
+
         String string = "1.4.2-beta23.1-SNAPSHOT";
 
         Version version = interactiveVersionReader.parseVersion(string);
@@ -69,6 +71,21 @@ public class InteractiveVersionReaderTest {
                 .withMinor(4L)
                 .withPatch(2L)
                 .withLabel("beta23.1-SNAPSHOT")
+                .build()));
+    }
+
+    @Test
+    public void shouldReadVersion() throws Exception {
+        String prompt = "please enter version";
+
+        Version version = interactiveVersionReader.readVersion(prompt);
+
+        verify(printStream, times(1)).print(eq(prompt));
+        assertThat(version, is(versionBuilder()
+                .withMajor(1L)
+                .withMinor(4L)
+                .withPatch(2L)
+                .withLabel("SNAPSHOT")
                 .build()));
 
     }
